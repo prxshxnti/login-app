893ea99dfeef44c2afbce71e8858ad3e3f3453ac
prxshxnti <prashantiramesh3@gmail.com>
2024-12-28 12:49:51 +0530

    Static version of login and register page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Form from "./components/Form";
+import Register from "./components/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex w-full h-screen">
+        <Routes>
+          {/* Sign In Route */}
+          <Route
+            path="/"
+            element={
+              <div className="w-full lg:w-1/2 flex items-center justify-center">
+                <div className="p-8 mt-10 lg:mt-20">
+                  <Form />
+                </div>
+              </div>
+            }
+          />
+          {/* Register Route */}
+          <Route
+            path="/register"
+            element={
+              <div className="w-full lg:w-1/2 flex items-center justify-center">
+                <div className="p-8 mt-10 lg:mt-20">
+                  <Register />
+                </div>
+              </div>
+            }
+          />
+        </Routes>
+
+        {/* Right Side Design for Both Pages */}
+        <div className="hidden relative lg:flex h-full items-center justify-center bg-gray-200 w-1/2">
+          <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-bounce" />
+          <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg" />
+        </div>
+      </div>
+    </Router>
   );
 }
 
